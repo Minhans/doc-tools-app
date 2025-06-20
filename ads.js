4e95c0825bca1db8d939ef09461e2bae2b8d0f56
Minhans <skmitansh94@gmail.com>
2025-06-20 07:54:59 +0530

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,5 @@
+// Example placeholder
+window.onload = () => {
+  const ad = document.getElementById("ad-space");
+  ad.innerHTML = `<iframe src="https://adsense.com/your-ad-code" width="300" height="250"></iframe>`;
+};
